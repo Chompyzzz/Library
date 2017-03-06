ed03a2650282c9eae5f088c1687ff29a9a005236
3f07 <3f07@Liceospalla.local>
2017-03-06 11:58:10 +0100

    first commit

---
+++ Library/Library/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class User
+    {
+        public string nome { get; set; }
+
+        public string cognome { get; set; }
+
+        public string cf { get; { return nome + cognome; } }
+
+        private int year { get; set; }
+        private int month { get; set; }
+        private int day { get; set; }
+
+
+        DateTime dt = new DateTime();
+
+    }
+}
